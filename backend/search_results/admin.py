e42bea985df6578c0588a24ccc61b87ecfcb7fe9
Keannu Bernasol <noehbernasol0@gmail.com>
2024-09-24 16:09:47 +0800

    Add timestamp filtering for search results in django admin

--- backend/search_results/admin.py
+++ backend/search_results/admin.py
@@ -1,10 +1,18 @@
 from unfold.admin import ModelAdmin
 from django.contrib import admin
 from .models import SearchResult
+from unfold.contrib.filters.admin import RangeDateFilter
 
 
 @admin.register(SearchResult)
 class SearchResultAdmin(ModelAdmin):
     model = SearchResult
     search_fields = ('id', 'title', 'link')
-    list_display = ['id', 'title']
+    list_display = ['id', 'title', 'timestamp']
+
+    list_filter_submit = True
+    list_filter = ((
+        "timestamp", RangeDateFilter
+    ), (
+        "timestamp", RangeDateFilter
+    ),)
