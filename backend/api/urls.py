080bebbb5d33f03b0c7d4981185597878621b1c4
Keannu Bernasol <noehbernasol0@gmail.com>
2024-09-25 19:27:02 +0800

    Move all URLs behind /api/v1/

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,21 @@
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.contrib import admin
 from config.settings import DEBUG, SERVE_MEDIA, MEDIA_ROOT
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('subscriptions/', include('subscriptions.urls')),
     path('notifications/', include('notifications.urls')),
     path('billing/', include('billing.urls')),
-    path('stripe/', include('payments.urls'))
+    path('stripe/', include('payments.urls')),
+    path('admin/', admin.site.urls),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('swagger/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 # URLs for local development
@@ -15,3 +23,5 @@ if DEBUG and SERVE_MEDIA:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(
         'media/', document_root=MEDIA_ROOT)
+if DEBUG:
+    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,17 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-from config.settings import DEBUG
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/',
-         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/',
-         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
-
-if DEBUG:
-    urlpatterns += [path('silk/', include('silk.urls', namespace='silk'))]
