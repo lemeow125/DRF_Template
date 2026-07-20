09b41626bd09e1f8e48e38e6652125eb230b2e0a
Keannu Bernasol <noehbernasol0@gmail.com>
2026-07-20 09:21:14 +0800

    Use correct envs in pre deployment tests

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -38,9 +38,9 @@ class Config:
                 setattr(self, field_name, self._generate_faker_data(field_name, field_info))
 
         elif os.getenv("BACKEND_GENERATE_CONFIG", "false").lower() == "true":
-            # Generate fakes, but skip ADMIN_EMAIL and PASSWORD in CI/pre-deployment
+            # Generate fakes, but skip DEBUG_USER_EMAIL and DEBUG_USER_PASSWORD in CI/pre-deployment
             for field_name, field_info in ConfigModel.model_fields.items():
-                if field_name in ("ADMIN_EMAIL", "PASSWORD"):
+                if field_name in ("DEBUG_USER_EMAIL", "DEBUG_USER_PASSWORD"):
                     value = self.set_env_var((field_name, field_info))
                 else:
                     value = self._generate_faker_data(field_name, field_info)
